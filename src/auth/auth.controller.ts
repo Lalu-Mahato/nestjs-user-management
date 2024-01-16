6d6744acf0d1ab991a8dcb7bacd23b5f9b30884a
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2024-01-16 16:59:05 +0530

    Removed unneccessary code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,10 @@
-import { Controller, Post, Body, Req, Get, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from './entities/user.entity';
 import { ApiResponse } from 'src/common/types/common.types';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoginResponse } from './types/common.types';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -23,12 +22,5 @@ export class AuthController {
     @Body() loginUserDto: LoginUserDto,
   ): Promise<ApiResponse<LoginResponse>> {
     return this.authService.login(loginUserDto);
-  }
-
-  @Get('/test')
-  @UseGuards(AuthGuard())
-  test(@Req() req) {
-    console.log(req);
-    return 'Hello World';
   }
 }
